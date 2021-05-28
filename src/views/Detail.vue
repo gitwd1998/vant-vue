6c34e15fad2a01fb700dca7d93a803bcec03c2f7
gitwd1998 <wd15855385074@outlook.com>
2021-05-28 09:33:41 +0800

    ADD 添加日记删除编辑功能

--- src/views/Detail.vue
+++ src/views/Detail.vue
@@ -12,16 +12,27 @@
       maxlength="100"
       show-word-limit
     ></van-filed>
+    <van-button
+      round
+      block
+      type="primary"
+      @click="$router.push({ path: '/notes/edit', query: { id } })"
+      >编 辑</van-button
+    >
+    <van-button round block type="danger" @click="handleDelete"
+      >删 除</van-button
+    >
     <!-- <div>{{ note.time | dealTime }}</div> 过滤器的用法-->
   </div>
 </template>
 <script>
-import { Field } from "vant";
-import { detail } from "@/api";
+import { Button, Field, Dialog, Toast } from "vant";
+import { detail, deleteItem } from "@/api";
 import { dealTime } from "@/utils/tools";
 export default {
   components: {
     vanFiled: Field,
+    vanButton: Button,
   },
   filters: {
     dealTime: function (value) {
@@ -31,11 +42,7 @@ export default {
   data() {
     return {
       id: this.$route.query.id,
-      note: {
-        title: "",
-        content: "",
-        time: "",
-      },
+      note: {},
     };
   },
   created() {
@@ -48,6 +55,25 @@ export default {
       this.note = data;
       this.note.time = dealTime(data.time);
     },
+    handleDelete() {
+      Dialog.confirm({
+        title: "提示信息",
+        message: "确定要删除当前日记吗？",
+      })
+        .then(async () => {
+          await deleteItem({ id: this.id });
+          this.$router.push({ path: "/notes/lists" });
+          Toast.success("删除成功");
+        })
+        .catch(() => {
+          Toast.fail("取消删除");
+        });
+    },
   },
 };
 </script>
+<style lang="less" scoped>
+.van-button {
+  margin-bottom: 20px;
+}
+</style>
