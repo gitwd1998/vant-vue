ea7c6fac6febd18713e782421898b4f3bd16b6d6
gitwd1998 <wd15855385074@outlook.com>
2021-08-25 16:21:39 +0800

    MOD 调试WebSocKer

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,3 +1,23 @@
 <template>
   <div class="main-content"></div>
 </template>
+<script>
+export default {
+  mounted() {
+    // const ws = new WebSocket("ws://127.0.0.1:9000");
+    // ws.onopen = (e) => {
+    //   console.log(e);
+    //   ws.send("hello WebSockets...");
+    // };
+    // ws.onmessage = (e) => {
+    //   console.log(e);
+    // };
+    // ws.onclose = (e) => {
+    //   console.log(e);
+    // };
+    // ws.onerror = (e) => {
+    //   console.log(e);
+    // };
+  },
+};
+</script>
