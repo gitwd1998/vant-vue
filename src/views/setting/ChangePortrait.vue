0197dafc65b34d9831d886b11a399a6024970571
gitwd1998 <wd15855385074@outlook.com>
2021-08-25 16:08:46 +0800

    MOD 设置页面

---
+++ src/views/setting/ChangePortrait.vue
@@ -0,0 +1,35 @@
+<template>
+  <van-uploader :after-read="afterRead">
+    <van-button icon="plus" type="primary">上传文件</van-button>
+  </van-uploader>
+</template>
+<script>
+import { uploadHead } from "@/api";
+import { Button, Uploader, Toast } from "vant";
+export default {
+  components: {
+    vanUploader: Uploader,
+    vanButton: Button,
+  },
+  methods: {
+    async afterRead({ file }) {
+      let formData = new FormData();
+      formData.append("file", file);
+      let { data } = await uploadHead(formData);
+      console.log(data);
+      if (data.code === "0") {
+        this.$store.commit("setHead", data.data.url);
+        Toast({
+          message: data.msg,
+          duration: 1000,
+        });
+        setTimeout(() => {
+          this.$router.replace({ path: "/setting" });
+        }, 1000);
+      } else {
+        Toast("修改失败");
+      }
+    },
+  },
+};
+</script>
---
+++ src/views/setting/Info.vue
@@ -0,0 +1,66 @@
+<template>
+  <van-cell-group>
+    <van-cell
+      title="头像"
+      center
+      icon="smile-o"
+      @click="$router.push({ path: '/setting/changePortrait' })"
+    >
+      <template #extra>
+        <van-image
+          width="50"
+          height="50"
+          round
+          fit="cover"
+          :src="require('@/assets/img/logo.png')"
+        />
+      </template>
+    </van-cell>
+    <van-cell title="用户名" :value="username" icon="user-o" />
+    <van-cell title="手机号码" :value="phone" icon="phone-o" />
+    <van-cell
+      title="修改密码"
+      is-link
+      @click="$router.push({ path: '/setting/changePassword' })"
+    />
+    <van-cell title="退出登录" is-link @click="onExit" />
+    <div>{{ head }}</div>
+  </van-cell-group>
+</template>
+<script>
+import { Cell, CellGroup, Image, Dialog } from "vant";
+export default {
+  components: {
+    vanCellGroup: CellGroup,
+    vanCell: Cell,
+    vanImage: Image,
+  },
+  data() {
+    return {};
+  },
+  computed: {
+    username() {
+      return this.$store.getters.getUser;
+    },
+    phone() {
+      return this.$store.getters.getPhone;
+    },
+    head() {
+      return this.$store.getters.getHead;
+    },
+  },
+  methods: {
+    onExit() {
+      Dialog.confirm({
+        title: "提示信息",
+        message: "确定要退出登录吗？",
+      })
+        .then(() => {
+          localStorage.clear();
+          this.$router.replace({ path: "/login" });
+        })
+        .catch(() => {});
+    },
+  },
+};
+</script>
